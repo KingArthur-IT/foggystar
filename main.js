1ad007ecb1a6af7b559eed78ff19d6196369067d
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-12-09 15:05:19 +0300

    modal

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 const spinner = document.querySelector('#spinner');
 const spinnerBtn = document.querySelector('.spinner__btn');
 const animTimeout = 2000;
+const modal = document.querySelector('.modal');
+
+
+const openModal = () => {
+    if (modal){
+        modal.classList.add('displayed');
+        document.querySelector('body').classList.add('overflow-hidden');
+        setTimeout(() => {
+            modal.classList.add('visible');
+        }, 300);
+    }
+}
+
+const closeModal = () => {
+    if (modal){
+        modal.classList.remove('visible');
+        document.querySelector('body').classList.remove('overflow-hidden');
+        setTimeout(() => {
+            modal.classList.remove('displayed');
+        }, 300);
+    }
+};
 
 spinnerBtn.addEventListener('click', () => {
-    spinner.classList.add('spinning');
-    setTimeout(() => {
-        spinner.classList.add('spinned');
-        spinnerBtn.classList.add('spinned');
-        spinnerBtn.innerHTML = 'get bonus';
-    }, animTimeout);
-})
+    const isSpinned = spinnerBtn.classList.contains('spinned');
+
+    if (!isSpinned){
+        spinner.classList.add('spinning'); //spinning
+        setTimeout(() => { //change btn & add sparks
+            spinner.classList.add('spinned');
+            spinnerBtn.classList.add('spinned');
+            spinnerBtn.innerHTML = 'get bonus';
+        }, animTimeout);
+    }
+    else openModal();
+});
+
+document.querySelector('.modal__close')?.addEventListener('click', () => closeModal());
+modal.addEventListener('click', () => closeModal());
+
+document.querySelector('.modal__hero').addEventListener('click', e => e.stopPropagation())
